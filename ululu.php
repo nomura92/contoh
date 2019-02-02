e433f1de1c2897819a19761f2d06a21ec3a3db9e
Hendra Randy Nomura <hendra.randy.nomura92@gmail.com>
2019-02-02 15:29:34 +0700

    Add files via upload

---
+++ ululu.php
@@ -0,0 +1,73 @@
+<?php
+$aa= array(13) {
+  ["kecamatan"]=>
+  string(7) "1371090"
+  ["alamat"]=>
+  string(14) "Kampung Kalawi"
+  ["no_telp"]=>
+  string(6) "no1234"
+  ["kategori"]=>
+  string(1) "1"
+  ["jenis_member"]=>
+  string(1) "1"
+  ["password"]=>
+  string(6) "p12345"
+  ["call_center"]=>
+  string(6) "cc1234"
+  ["hari_buka"]=>
+  array(6) {
+    [0]=>
+    string(5) "Senin"
+    [1]=>
+    string(6) "Selasa"
+    [2]=>
+    string(4) "Rabu"
+    [3]=>
+    string(5) "Kamis"
+    [4]=>
+    string(5) "Jumat"
+    [5]=>
+    string(5) "Sabtu"
+  }
+  ["jam_buka"]=>
+  array(15) {
+    [0]=>
+    string(1) "8"
+    [1]=>
+    string(1) "9"
+    [2]=>
+    string(2) "10"
+    [3]=>
+    string(2) "11"
+    [4]=>
+    string(2) "12"
+    [5]=>
+    string(2) "13"
+    [6]=>
+    string(2) "14"
+    [7]=>
+    string(2) "15"
+    [8]=>
+    string(2) "16"
+    [9]=>
+    string(2) "17"
+    [10]=>
+    string(2) "18"
+    [11]=>
+    string(2) "19"
+    [12]=>
+    string(2) "20"
+    [13]=>
+    string(2) "21"
+    [14]=>
+    string(2) "22"
+  }
+  ["saldo"]=>
+  string(5) "50000"
+  ["lama"]=>
+  string(1) "2"
+  ["mulai"]=>
+  string(10) "2018-08-28"
+  ["berakhir"]=>
+  string(10) "2018-10-28"
+}
